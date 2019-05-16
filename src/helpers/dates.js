7ab9722dca84a70bb1f405c50972c20de1e2a50f
Ljubixonovich <vikvikvuk@gmail.com>
2019-05-16 11:19:07 +0200

    task list is filtered

--- src/helpers/dates.js
+++ src/helpers/dates.js
@@ -1,10 +1,15 @@
 
 export const getFormatedDate = (date) => {
- //  console.log('getFormatedDate: ' + date);
    let dd = String(date.getDate()).padStart(2, '0');
-   let mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+   let mm = String(date.getMonth() + 1).padStart(2, '0'); 
    let yyyy = date.getFullYear();
 
    let today = `${dd}. ${mm}. ${yyyy}.`; 
    return today;
+}
+
+export const compareDates = (date1, date2) => {
+   if (date1 > date2) return false;
+   else if (date1 < date2) return false;
+   else return true;
 }
--- src/screens/Day/Day.js
+++ src/screens/Day/Day.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, Button, FlatList, ScrollView } from 'react-native';
+import { View, Text, Button, FlatList } from 'react-native';
 import { connect } from 'react-redux';
 
-import { getFormatedDate } from '../../helpers/dates';
+import { getFormatedDate, compareDates } from '../../helpers/dates';
 import { mockTasks } from '../../helpers/mockTasks';
 
 export class DayScreen extends Component {
@@ -14,7 +14,6 @@ export class DayScreen extends Component {
 
    constructor(props) {
       super(props);
-    //  console.log('constructor ' + JSON.stringify(props));
    }
 
    nextDayHandler = () => {
@@ -45,27 +44,26 @@ export class DayScreen extends Component {
       });
    }
 
+   
+
 
    render() {
-      let tasks = mockTasks.filter(t => t.date == this.props.date);
-      console.log('mock task date: ' + mockTasks[4].date);
-      console.log('this.props.date: ' + this.props.date);
+      let tasks = mockTasks.filter(t => compareDates(t.date, this.props.date));
       return (
-         <View style={{flex: 1}}>
-            <View style={{flex: 1}}>
+         <View style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
                <Text> Day: {this.state.displayDate} </Text>
                <Button title='Next Day' onPress={this.nextDayHandler}></Button>
                <Button title='Previous Day' onPress={this.previousDayHandler}></Button>
             </View>
-            <View style={{flex: 2}}>
-               <FlatList 
+            <View style={{ flex: 2 }}>
+               <FlatList
                   data={tasks}
-                //  data={mockTasks}
                   keyExtractor={(task) => task.id}
                   showsVerticalScrollIndicator={false}
                   renderItem={(task) => {
                      return (
-                        <View style={{flex: 1}}>
+                        <View style={{ flex: 1 }}>
                            <Text>{getFormatedDate(task.item.date)}</Text>
                            <Text>{task.item.id}</Text>
                            <Text>{task.item.important ? 'Important' : 'Not important'}</Text>
