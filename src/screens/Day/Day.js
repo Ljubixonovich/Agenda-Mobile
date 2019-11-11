fbdbd9577851a37ebbdbb7c87b5dd37303f0b48a
Ljubixonovich <vikvikvuk@gmail.com>
2019-11-11 17:55:21 +0100

    remove class component from day.js

--- src/screens/Day/Day.js
+++ src/screens/Day/Day.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, FlatList, StyleSheet, Alert, TouchableOpacity, Image } from 'react-native';
 import { connect } from 'react-redux';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -7,83 +7,72 @@ import { getSerbianDate } from '../../helpers/dates';
 import Txt from '../../components/UI/MainText';
 import ListItemTask from '../../components/ListItemTask/ListItemTask';
 
-export class DayScreen extends Component {
-   
-   constructor(props) {
-      super(props);
-   }
+export const DayScreen = (props) => {
 
+   const { tasks, date } = props;
 
    onAddTaskHandler = () => {
-      this.props.navigator.showModal({
+      props.navigator.showModal({
          screen: 'agenda.TaskFormScreen',
          title: 'Add Task',
          animationType: 'fade',
-         passProps: {editMode: false, date: this.props.date, sendFrom: 'DayScreen'},
-         });
+         passProps: { editMode: false, date: date, sendFrom: 'DayScreen' },
+      });
    }
 
    onEditTaskHandler = (task) => {
-      console.log(task);
-      
-      this.props.navigator.showModal({
+      props.navigator.showModal({
          screen: 'agenda.TaskFormScreen',
          title: 'Edit Task',
          animationType: 'fade',
-         passProps: {editMode: true, task: task, date: this.props.date, sendFrom: 'DayScreen'},
+         passProps: { editMode: true, task: task, date: date, sendFrom: 'DayScreen' },
       });
    }
 
    connfirmDeleteTask = (id) => {
       Alert.alert('', 'Delete task ?', [
-         {text: 'no'},
-         {text: 'yes', onPress: () => this.props.onDeleteTask(id)}
-        
+         { text: 'no' },
+         { text: 'yes', onPress: () => props.onDeleteTask(id) }
+
       ]);
    }
 
 
-   render() {
-      const { tasks, date } = this.props;
 
-      return (
-         <View style={{ flex: 1, marginBottom: 20 }}>
-            <View style={styles.headerContainer}>
-               <View>
-                  <Image style={styles.icon}/>
-               </View>
-               <Txt style={styles.mainTitle}>
-                  {getSerbianDate(date)}
-               </Txt>
-               <TouchableOpacity  style={{padding: 14}} onPress={this.onAddTaskHandler}>
-                  {/* <Image source={require('../../assets/new.png')} 
-                     style={styles.icon} 
-                  /> */}
-                  <Icon name="md-add" size={30} color="#0AD217" />
-               </TouchableOpacity>
+   return (
+      <View style={{ flex: 1, marginBottom: 20 }}>
+         <View style={styles.headerContainer}>
+            <View>
+               <Image style={styles.icon} />
             </View>
-            <View style={{ flex: 5 }}>
-               {tasks.length > 0 &&
-                  <FlatList
-                     data={tasks}
-                     keyExtractor={(task) => task.id}
-                     showsVerticalScrollIndicator={false}
-                     renderItem={(t) => {
-                        let task = t.item;
-                        return (  
-                           <ListItemTask 
-                              task={task}
-                              onEditTaskHandler={this.onEditTaskHandler}
-                              connfirmDeleteTask={this.connfirmDeleteTask}
-                           />
-                        );
-                     }}
-                  />
-               }
-            </View>
+            <Txt style={styles.mainTitle}>
+               {getSerbianDate(date)}
+            </Txt>
+            <TouchableOpacity style={{ padding: 14 }} onPress={onAddTaskHandler}>
+               <Icon name="md-add" size={30} color="#0AD217" />
+            </TouchableOpacity>
          </View>
-      )
-   }
+         <View style={{ flex: 5 }}>
+            {tasks.length > 0 &&
+               <FlatList
+                  data={tasks}
+                  keyExtractor={(task) => task.id}
+                  showsVerticalScrollIndicator={false}
+                  renderItem={(t) => {
+                     let taskItem = t.item;
+                     return (
+                        <ListItemTask
+                           task={taskItem}
+                           onEditTaskHandler={onEditTaskHandler}
+                           connfirmDeleteTask={connfirmDeleteTask}
+                        />
+                     );
+                  }}
+               />
+            }
+         </View>
+      </View>
+   )
 }
 
 const styles = StyleSheet.create({
@@ -91,9 +80,8 @@ const styles = StyleSheet.create({
       flexDirection: 'row',
       justifyContent: 'space-between',
       alignItems: 'center',
-      // marginBottom: 10,
       borderBottomWidth: 1,
-      borderBottomColor: '#bbb', 
+      borderBottomColor: '#bbb',
    },
    mainTitle: {
       fontSize: 20,
@@ -101,19 +89,17 @@ const styles = StyleSheet.create({
       textAlign: 'center',
       color: 'black',
    },
-   icon: {     
-      height: 20, 
+   icon: {
+      height: 20,
       width: 20
    },
 })
 
 
 const mapStateToProps = (state) => ({
-
 })
 
 const mapDispatchToProps = {
-
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(DayScreen);
