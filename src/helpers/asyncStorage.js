591ff5359d3e4a17d4738d002490eed3cb7ba205
Ljubomir Vukelic <ljubomir.vukelic@gmail.com>
2022-07-14 15:49:49 +0200

    predlog ubrzanja getTasks iz asyncStorage

--- src/helpers/asyncStorage.js
+++ src/helpers/asyncStorage.js
@@ -1,63 +1,80 @@
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from "@react-native-community/async-storage";
 
 export const getLastId = async () => {
-   try {
-      let value = await AsyncStorage.getItem('@lastId');
-      value = !value ? 0 : value;
-      return value;
-   } catch (e) {
-      console.log('error with getLastId');
-   }
-}
+  try {
+    let value = await AsyncStorage.getItem("@lastId");
+    value = !value ? 0 : value;
+    return value;
+  } catch (e) {
+    console.log("error with getLastId");
+  }
+};
 
 export const addTask = async (task) => {
-   try {
-      let lastId = await getLastId();
-      lastId = parseInt(lastId);
-      task.id = (lastId + 1).toString();
-      await AsyncStorage.setItem('@' + ((lastId + 1).toString()), JSON.stringify(task));
-      await AsyncStorage.setItem('@lastId', (lastId + 1).toString());
-      console.log('Task added.');
-   } catch (e) {
-      console.log('error with adding task');
-   }
-}
+  try {
+    let lastId = await getLastId();
+    lastId = parseInt(lastId);
+    task.id = (lastId + 1).toString();
+    await AsyncStorage.setItem(
+      "@" + (lastId + 1).toString(),
+      JSON.stringify(task)
+    );
+    await AsyncStorage.setItem("@lastId", (lastId + 1).toString());
+    console.log("Task added.");
+  } catch (e) {
+    console.log("error with adding task");
+  }
+};
 
 export const editTask = async (task) => {
-   try {
-      const value = await AsyncStorage.setItem('@' + (task.id).toString(), JSON.stringify(task));
-      console.log('Task edited.');
-   } catch (e) {
-      console.log('error with editing task');
-   }   
-}
+  try {
+    const value = await AsyncStorage.setItem(
+      "@" + task.id.toString(),
+      JSON.stringify(task)
+    );
+    console.log("Task edited.");
+  } catch (e) {
+    console.log("error with editing task");
+  }
+};
 
 export const deleteTask = async (id) => {
-   try {
-      await AsyncStorage.removeItem('@' + id);
-      console.log('Task deleted.');
-   } catch (e) {
-      console.log('error with deleting task');
-   }
-   
-}
+  try {
+    await AsyncStorage.removeItem("@" + id);
+    console.log("Task deleted.");
+  } catch (e) {
+    console.log("error with deleting task");
+  }
+};
 
 export const getAllTasks = async () => {
-   let keys, tasks = [];
-   try {
-      keys = await AsyncStorage.getAllKeys();
-      for (const k of keys) {
-         // get only tasks
-         if (k !== '@lastId') {
-            const task = await AsyncStorage.getItem(k);
-            if (task !== null) {
-               tasks.push(JSON.parse(task));
-            }
-         }
+  let keys,
+    tasks = [];
+  try {
+    keys = await AsyncStorage.getAllKeys();
+    for (const k of keys) {
+      // get only tasks
+      if (k !== "@lastId") {
+        const task = await AsyncStorage.getItem(k);
+        if (task !== null) {
+          tasks.push(JSON.parse(task));
+        }
       }
-      console.log('tasks.lenght: ' + tasks.lenght);
-      return tasks;
-   } catch (error) {
-      console.log('error with getAllTasks');
-   }
-}
+    }
+    console.log("tasks.lenght: " + tasks.lenght);
+    return tasks;
+  } catch (error) {
+    console.log("error with getAllTasks");
+  }
+};
+
+// TODO sve metode promenuti da prihvataju tasks: [], a ne pojedinacne taskIteme
+// export const getAllTasks = async () => {
+//   try {
+//     const task = await AsyncStorage.getItem("@tasks");
+//     console.log("tasks.lenght: " + tasks.lenght);
+//     return !task ? [] : JSON.parse(tasks);
+//   } catch (error) {
+//     console.log("error with getAllTasks");
+//   }
+// };
